e77c8a1dd7e6057b2d0e56a1ae2832a7585d23a0
kantyadav1817@gmail.com <kantyadav1817@gmail.com>
2025-12-18 11:26:50 +0530

    Fix MySQL connection for Render using Railway env variables

--- db.js
+++ db.js
@@ -1,18 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "kantyadav18",
-  database: "expense_tracker"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("DB error");
+    console.error("DB connection error ❌", err);
     return;
   }
-  console.log("MySQL connected");
+  console.log("MySQL connected successfully ✅");
 });
 
 module.exports = db;
